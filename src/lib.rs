00342f189e928adb0348ffac1cadeaf76870ae4a
David Tolnay <dtolnay@gmail.com>
2025-12-21 09:19:35 -0800

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! [![github]](https://github.com/dtolnay/opt-level)&ensp;[![crates-io]](https://crates.io/crates/opt-level)&ensp;[![docs-rs]](https://docs.rs/opt-level)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
+//! <br>
+//!
 //! Get the value of rustc's `-Copt-level=` flag at runtime.
 //!
 //! Useful for sizing tests to run fewer iterations in slow build modes.
